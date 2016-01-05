32f81338cb7efd412b2c2b4073d5a7b19a814953
Andreas Beasley <andreas@sapioweb.com>
2016-01-04 23:36:32 -0800

    url methods on auth links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,15 +53,15 @@
             <div class="collapse navbar-collapse" id="spark-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="/">Home</a></li>
+                    <li><a href="{{route('index')}}">Home</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="/login">Login</a></li>
-                        <li><a href="/register">Register</a></li>
+                        <li><a href="{{url('/login')}}">Login</a></li>
+                        <li><a href="{{url('/register')}}">Register</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -69,7 +69,7 @@
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="/logout"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                <li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                     @endif
                 </ul>
